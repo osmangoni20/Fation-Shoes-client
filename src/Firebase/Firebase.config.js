f3e2fa78ca1f5d4d8f573332dcd1190785d3fb40
Osman <osmangoni0827@gmail.com>
2024-06-05 23:38:45 +0600

    update

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -2,18 +2,18 @@
 import { initializeApp } from "firebase/app";
 console.log(import.meta.env.local)
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
-  // apiKey: "AIzaSyBmZvn56dijD-lOTdeuT2AQnvJ7x9Hq_-8",
-  // authDomain: "strid-46e89.firebaseapp.com",
-  // projectId: "strid-46e89",
-  // storageBucket: "strid-46e89.appspot.com",
-  // messagingSenderId: "467073263611",
-  // appId: "1:467073263611:web:59fa6f662f9048d80b0d5e"
+  // apiKey: import.meta.env.VITE_apiKey,
+  // authDomain: import.meta.env.VITE_authDomain,
+  // projectId: import.meta.env.VITE_projectId,
+  // storageBucket: import.meta.env.VITE_storageBucket,
+  // messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  // appId: import.meta.env.VITE_appId
+  apiKey: "AIzaSyBmZvn56dijD-lOTdeuT2AQnvJ7x9Hq_-8",
+  authDomain: "strid-46e89.firebaseapp.com",
+  projectId: "strid-46e89",
+  storageBucket: "strid-46e89.appspot.com",
+  messagingSenderId: "467073263611",
+  appId: "1:467073263611:web:59fa6f662f9048d80b0d5e"
 };
 
 
